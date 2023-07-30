faca6378730147df0c547fa8d5a12ce04f402a22
Atta ur Rehman <attaurr786@outlook.com>
2023-07-30 12:31:12 +0500

    usermodel created and connected with mongo data base

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+// const User = require('./models/User')
+const User = require('./models/User')
 
 app.use(cors());
 app.use(express.json());
+
+
+mongoose.connect('mongodb+srv://adminblog:KUdPUXGTiUCBocwC@cluster0.kvfoaml.mongodb.net/?retryWrites=true&w=majority')
 app.post('/register',  (req,res)=> 
 {
     const {username, password} = req.body;
-    res.json({RequestData: {username, password}});
+    const userDoc = User.create({username, password});
+    res.json(userDoc);
 })
 
 app.listen(4000);
