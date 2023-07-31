880a98bb490bd3e50e53b479975eb18224ed9967
Atta ur Rehman <attaurr786@outlook.com>
2023-07-31 21:23:14 +0500

    login jsx done

--- client/src/Pages/loginpage.jsx
+++ client/src/Pages/loginpage.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react"
+
 export default function LoginPage()
 {
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+async function login(ev)
+{
+    ev.preventDefault();
+    await fetch ('http://localhost:5173/login',
+    { method: 'POST',
+        body:JSON.stringify({username,password}),
+        header:{'content:type':'application/json'},
+
+    })
+}
     return (
-       <form action="" className="login">
+       <form action="" className="login" onSubmit={login}>
                 <h2>Login</h2>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
+        <input type="text" placeholder="username" value={username} onChange={ev=> setUsername(ev.target.value)}/>
+        <input type="password" placeholder="password" value={password} onChange={ev=> setPassword(ev.target.value)}/>
         <button>Login</button>
        </form>
     )
